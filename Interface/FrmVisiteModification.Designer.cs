934c4f67c84ffe6a2dfd58ee761bf38566777a3e
Jeremy ZON <jzon@stremi.fr>
2026-03-13 16:13:19 +0100

    Inital Commit

---
+++ Interface/FrmVisiteModification.Designer.cs
@@ -0,0 +1,204 @@
+﻿namespace Interface
+{
+    partial class FrmVisiteModification
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(FrmVisiteModification));
+            panelDroite = new Panel();
+            btnModifier = new Button();
+            dtpDate = new DateTimePicker();
+            label4 = new Label();
+            lblDate = new Label();
+            label2 = new Label();
+            lblNom = new Label();
+            label1 = new Label();
+            panelGauche = new Panel();
+            dgvVisites = new DataGridView();
+            label3 = new Label();
+            panelDroite.SuspendLayout();
+            panelGauche.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvVisites).BeginInit();
+            SuspendLayout();
+            // 
+            // lblTitre
+            // 
+            lblTitre.Size = new Size(1281, 74);
+            // 
+            // panelDroite
+            // 
+            panelDroite.Controls.Add(btnModifier);
+            panelDroite.Controls.Add(dtpDate);
+            panelDroite.Controls.Add(label4);
+            panelDroite.Controls.Add(lblDate);
+            panelDroite.Controls.Add(label2);
+            panelDroite.Controls.Add(lblNom);
+            panelDroite.Controls.Add(label1);
+            panelDroite.Dock = DockStyle.Right;
+            panelDroite.Location = new Point(855, 98);
+            panelDroite.Name = "panelDroite";
+            panelDroite.Size = new Size(426, 370);
+            panelDroite.TabIndex = 13;
+            // 
+            // btnModifier
+            // 
+            btnModifier.BackColor = Color.Red;
+            btnModifier.Location = new Point(234, 236);
+            btnModifier.Name = "btnModifier";
+            btnModifier.Size = new Size(96, 30);
+            btnModifier.TabIndex = 6;
+            btnModifier.Text = "Modifier";
+            btnModifier.UseVisualStyleBackColor = false;
+            btnModifier.Click += btnModifier_Click;
+            // 
+            // dtpDate
+            // 
+            dtpDate.CustomFormat = "dd/MM/yyyy HH:mm";
+            dtpDate.Format = DateTimePickerFormat.Custom;
+            dtpDate.Location = new Point(43, 198);
+            dtpDate.Name = "dtpDate";
+            dtpDate.Size = new Size(200, 23);
+            dtpDate.TabIndex = 5;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(43, 168);
+            label4.Name = "label4";
+            label4.Size = new Size(65, 15);
+            label4.TabIndex = 4;
+            label4.Text = "est remi au";
+            // 
+            // lblDate
+            // 
+            lblDate.AutoSize = true;
+            lblDate.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblDate.Location = new Point(43, 138);
+            lblDate.Name = "lblDate";
+            lblDate.Size = new Size(175, 15);
+            lblDate.TabIndex = 3;
+            lblDate.Text = "Date et heure du rendez-vous";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(43, 108);
+            label2.Name = "label2";
+            label2.Size = new Size(115, 15);
+            label2.TabIndex = 2;
+            label2.Text = "prévu initialement le";
+            // 
+            // lblNom
+            // 
+            lblNom.AutoSize = true;
+            lblNom.Font = new Font("Segoe UI", 9F, FontStyle.Bold, GraphicsUnit.Point, 0);
+            lblNom.Location = new Point(43, 78);
+            lblNom.Name = "lblNom";
+            lblNom.Size = new Size(103, 15);
+            lblNom.TabIndex = 1;
+            lblNom.Text = "Nom du praticien";
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(43, 48);
+            label1.Name = "label1";
+            label1.Size = new Size(114, 15);
+            label1.TabIndex = 0;
+            label1.Text = "Le rendez-vous chez";
+            // 
+            // panelGauche
+            // 
+            panelGauche.Controls.Add(dgvVisites);
+            panelGauche.Controls.Add(label3);
+            panelGauche.Dock = DockStyle.Fill;
+            panelGauche.Location = new Point(0, 98);
+            panelGauche.Name = "panelGauche";
+            panelGauche.Padding = new Padding(10);
+            panelGauche.Size = new Size(855, 370);
+            panelGauche.TabIndex = 14;
+            // 
+            // dgvVisites
+            // 
+            dgvVisites.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dgvVisites.Dock = DockStyle.Fill;
+            dgvVisites.Location = new Point(10, 25);
+            dgvVisites.Name = "dgvVisites";
+            dgvVisites.Size = new Size(835, 335);
+            dgvVisites.TabIndex = 1;
+            dgvVisites.SelectionChanged += dgvVisites_SelectionChanged;
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Dock = DockStyle.Top;
+            label3.Location = new Point(10, 10);
+            label3.Name = "label3";
+            label3.Size = new Size(335, 15);
+            label3.TabIndex = 0;
+            label3.Text = "Sélectionner une visite afin de modifier la date du rendez-vous";
+            // 
+            // FrmVisiteModification
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1281, 513);
+            Controls.Add(panelGauche);
+            Controls.Add(panelDroite);
+            Icon = (Icon)resources.GetObject("$this.Icon");
+            KeyPreview = true;
+            Location = new Point(0, 0);
+            Name = "FrmVisiteModification";
+            Text = "FrmVisiteModification";
+            Load += FrmVisiteModification_Load;
+            Controls.SetChildIndex(lblTitre, 0);
+            Controls.SetChildIndex(panelDroite, 0);
+            Controls.SetChildIndex(panelGauche, 0);
+            panelDroite.ResumeLayout(false);
+            panelDroite.PerformLayout();
+            panelGauche.ResumeLayout(false);
+            panelGauche.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)dgvVisites).EndInit();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Panel panelDroite;
+        private Label label1;
+        private Panel panelGauche;
+        private Label label4;
+        private Label lblDate;
+        private Label label2;
+        private Label lblNom;
+        private Button btnModifier;
+        private DateTimePicker dtpDate;
+        private DataGridView dgvVisites;
+        private Label label3;
+    }
+}
---
+++ Interface/FrmVisiteModification.cs
@@ -0,0 +1,305 @@
+﻿using Donnee;
+using Interface.Properties;
+using Metier;
+using System.Data;
+
+namespace Interface
+{
+    public partial class FrmVisiteModification : FrmBase
+    {
+        public FrmVisiteModification(Session uneSession) : base(uneSession)
+        {
+            InitializeComponent();
+        }
+
+        #region procédures événementielles
+
+        private void FrmVisiteModification_Load(object sender, EventArgs e)
+        {
+            parametrerComposant();
+            remplirDgv();
+        }
+
+        private void dgvVisites_SelectionChanged(object sender, EventArgs e)
+        {
+            afficherVisite();
+        }
+
+        private void dgvVisites_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+            if (e.RowIndex < 0) return;
+            if (e.ColumnIndex == 1)
+            {
+                supprimer(e.RowIndex);
+            }
+        }
+
+        private void btnModifier_Click(object sender, EventArgs e)
+        {
+            modifier();
+        }
+
+        #endregion
+
+        #region procédures
+
+        private void parametrerComposant()
+        {
+            this.lblTitre.Text = "Modifier une visite";
+
+            dtpDate.Format = DateTimePickerFormat.Custom;
+            dtpDate.CustomFormat = "dd/MM/yyyy HH:mm";
+
+            parametrerDgv(dgvVisites);
+        }
+
+        private void parametrerDgv(DataGridView dgv)
+        {
+            dgv.Columns.Clear();
+            dgv.Rows.Clear();
+
+            #region paramètrage concernant le datagridview dans son ensemble
+
+            dgv.Enabled = true;
+            dgv.BorderStyle = BorderStyle.FixedSingle;
+            dgv.BackgroundColor = Color.White;
+            dgv.ForeColor = Color.Black;
+            dgv.DefaultCellStyle.Font = new Font("Georgia", 11);
+            dgv.SelectionMode = DataGridViewSelectionMode.FullRowSelect;
+            dgv.MultiSelect = false;
+            dgv.AllowUserToDeleteRows = false;
+            dgv.AllowUserToAddRows = false;
+            dgv.EditMode = DataGridViewEditMode.EditProgrammatically;
+            dgv.AllowUserToResizeColumns = false;
+            dgv.AllowUserToResizeRows = false;
+            dgv.AllowUserToOrderColumns = false;
+            dgv.AllowDrop = false;
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            #endregion
+
+            #region paramètrage concernant la ligne d'entête
+
+            dgv.ColumnHeadersVisible = true;
+            dgv.ColumnHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+            dgv.EnableHeadersVisualStyles = false;
+            DataGridViewCellStyle style = dgv.ColumnHeadersDefaultCellStyle;
+            style.BackColor = Color.WhiteSmoke;
+            style.ForeColor = Color.Black;
+            style.SelectionBackColor = Color.WhiteSmoke;
+            style.SelectionForeColor = Color.Black;
+            style.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            style.Font = new Font("Georgia", 12, FontStyle.Bold);
+            dgv.ColumnHeadersHeightSizeMode = DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            dgv.ColumnHeadersHeight = 40;
+
+            #endregion
+
+            #region paramètrage concernant l'entête de ligne
+
+            dgv.RowHeadersVisible = false;
+            dgv.RowHeadersBorderStyle = DataGridViewHeaderBorderStyle.None;
+
+            #endregion
+
+            #region paramètrage au niveau des lignes
+
+            dgv.RowTemplate.Height = 30;
+
+            #endregion
+
+            #region paramètrage au niveau des cellules
+
+            dgv.CellBorderStyle = DataGridViewCellBorderStyle.None;
+            dgv.RowsDefaultCellStyle.BackColor = Color.White;
+
+            #endregion
+
+            #region paramètrage au niveau de la zone sélectionnée
+
+            dgv.RowsDefaultCellStyle.SelectionBackColor = System.Drawing.Color.White;
+            dgv.RowsDefaultCellStyle.SelectionForeColor = System.Drawing.Color.Black;
+
+            #endregion
+
+            #region paramètrage des colonnes
+
+            dgv.AutoSizeColumnsMode = DataGridViewAutoSizeColumnsMode.Fill;
+
+            DataGridViewColumn col;
+
+            // Colonne 0 : objet Visite (cachée)
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Visite";
+            col.HeaderText = "";
+            col.Visible = false;
+            dgv.Columns.Add(col);
+
+            // Colonne 1 : image de suppression
+            DataGridViewImageColumn colImg = new DataGridViewImageColumn();
+            colImg.Name = "Supprimer";
+            colImg.HeaderText = "";
+            colImg.Width = 30;
+            colImg.Image = Resources.supprimer;
+            colImg.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            colImg.ImageLayout = DataGridViewImageCellLayout.Zoom;
+            dgv.Columns.Add(colImg);
+
+            // Colonne 2 : La date de la visite
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Date";
+            col.HeaderText = "Programmée le";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+            // Colonne 3 : heure du rendez-vous
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Heure";
+            col.HeaderText = "à";
+            col.Width = 50;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            col.HeaderCell.Style.Alignment = DataGridViewContentAlignment.MiddleCenter;
+            dgv.Columns.Add(col);
+
+            // Colonne 4 : ville du rendez-vous
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Lieu";
+            col.HeaderText = "sur";
+            col.Width = 200;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+            // Colonne 5 : praticien à rencontrer
+            col = new DataGridViewTextBoxColumn();
+            col.Name = "Praticien";
+            col.HeaderText = "chez";
+            col.Width = 250;
+            col.DefaultCellStyle.Alignment = DataGridViewContentAlignment.MiddleLeft;
+            dgv.Columns.Add(col);
+
+            for (int i = 0; i < dgv.ColumnCount; i++)
+                dgv.Columns[i].SortMode = DataGridViewColumnSortMode.NotSortable;
+
+            #endregion
+        }
+
+        private void remplirDgv()
+        {
+            dgvVisites.Rows.Clear();
+            foreach (Visite v in session.MesVisites.Where(v => v.Bilan is null).OrderBy(v => v.DateEtHeure))
+            {
+                dgvVisites.Rows.Add(
+                    v,
+                    Resources.supprimer,
+                    v.DateEtHeure.ToLongDateString(),
+                    v.DateEtHeure.ToShortTimeString(),
+                    v.LePraticien.Ville,
+                    v.LePraticien.NomPrenom);
+            }
+        }
+
+        private void afficherVisite()
+        {
+            if (dgvVisites.SelectedRows.Count == 0) return;
+
+            Visite? v = getVisiteSelectionnee();
+            if (v == null) return;
+
+            lblNom.Text = v.LePraticien.NomPrenom;
+            lblDate.Text = v.DateEtHeure.ToString("dd/MM/yyyy HH:mm");
+            dtpDate.Value = v.DateEtHeure;
+        }
+
+        private Visite? getVisiteSelectionnee()
+        {
+            if (dgvVisites.SelectedRows.Count == 0) return null;
+            return dgvVisites.SelectedRows[0].Cells["Visite"].Value as Visite;
+        }
+
+        private void supprimer(int rowIndex)
+        {
+            Visite? v = dgvVisites.Rows[rowIndex].Cells["Visite"].Value as Visite;
+            if (v == null) return;
+
+            DialogResult result = MessageBox.Show(
+                $"Voulez-vous vraiment supprimer la visite chez {v.LePraticien.NomPrenom} le {v.DateEtHeure:dd/MM/yyyy} ?",
+                "Confirmation",
+                MessageBoxButtons.YesNo,
+                MessageBoxIcon.Question);
+
+            if (result != DialogResult.Yes) return;
+
+            try
+            {
+                Passerelle.supprimerRendezVous(v.Id);
+                session.MesVisites.Remove(v);
+                remplirDgv();
+
+                lblNom.Text = "";
+                lblDate.Text = "";
+
+                MessageBox.Show("La visite a été supprimée avec succès.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Une erreur est survenue lors de la suppression : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private void modifier()
+        {
+            if (dgvVisites.SelectedRows.Count == 0)
+            {
+                MessageBox.Show("Veuillez sélectionner une visite à modifier.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (dtpDate.Value < DateTime.Now.AddHours(1))
+            {
+                MessageBox.Show("Veuillez sélectionner une date et une heure futures.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (dtpDate.Value.DayOfWeek == DayOfWeek.Sunday)
+            {
+                MessageBox.Show("Veuillez sélectionner une date qui n'est pas un dimanche.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (dtpDate.Value.Hour < 8 || dtpDate.Value.Hour >= 19)
+            {
+                MessageBox.Show("Veuillez sélectionner une heure entre 8 h et 19 h.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            if (dtpDate.Value > DateTime.Today.AddDays(60).AddHours(19))
+            {
+                MessageBox.Show("Veuillez sélectionner une date dans les 2 mois à venir.", "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return;
+            }
+
+            try
+            {
+                Visite? v = getVisiteSelectionnee();
+                if (v == null) return;
+
+                DateTime nouvelleDate = dtpDate.Value;
+
+                Passerelle.modifierRendezVous(v.Id, nouvelleDate);
+                v.deplacer(nouvelleDate);
+
+                remplirDgv();
+                afficherVisite();
+
+                MessageBox.Show("La visite a été modifiée avec succès.", "Succès", MessageBoxButtons.OK, MessageBoxIcon.Information);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Une erreur est survenue lors de la modification de la visite : " + ex.Message, "Erreur", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        #endregion
+    }
+}
